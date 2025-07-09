62dcc19f377c7a08477ad5c1c39b72a6f91be719
MANIMSV <manikandan.2002121@srec.ac.in>
2025-07-09 10:06:40 +0530

    Feat : Implement main window UI with link input and web view integration

--- mainwindow.py
+++ mainwindow.py
@@ -1,7 +1,10 @@
 # This Python file uses the following encoding: utf-8
 import sys
 
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow ,QVBoxLayout
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWebEngineCore import QWebEngineProfile
+from PySide6.QtCore import QUrl
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
@@ -9,12 +12,46 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 #     pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_MainWindow
 
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.browser = QWebEngineView(self)
+        self.profile = QWebEngineProfile.defaultProfile()
+        layout = QVBoxLayout(self.ui.page_1)
+        layout.addWidget(self.browser)
+        self.ui.stackedWidget.setCurrentWidget(self.ui.page_2)
+        self.browser.urlChanged.connect(self.on_url_changed)
+        self.browser.setUrl(QUrl("https://ft-eu.gehealthcare.com/EFTClient/Account/Login.htm"))
 
+    def on_url_changed(self, url):
+        print(f"Page navigated to: {url.toString()}")
+        if "/Home" in url.toString():
+            print("Logged in! Capturing cookies...")
+            self.dump_cookies()
+
+    def dump_cookies(self):
+        cookie_store = self.profile.cookieStore()
+        cookie_store.cookiesAdded.connect(self.save_cookies)
+        cookie_store.loadAllCookies()
+
+    def save_cookies(self, cookies):
+        cookies_saved = False
+        for cookie in cookies:
+            name = bytes(cookie.name()).decode()
+            value = bytes(cookie.value()).decode()
+            domain = cookie.domain()
+            path = cookie.path()
+            print(f"Cookie: {name}={value}; Domain={domain}; Path={path}")
+            with open("cookies.txt", "a") as f:
+                f.write(f"{name}={value}; domain={domain}; path={path}\n")
+            cookies_saved = True
+        if cookies_saved:
+            self.ui.stackedWidget.setCurrentWidget(self.ui.page_2)
+        else:
+            pass
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
---
+++ ui_form.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.9.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QLineEdit, QMainWindow,
+    QMenuBar, QPushButton, QSizePolicy, QStackedWidget,
+    QStatusBar, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.gridLayout_2 = QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.lineEdit_pastelink = QLineEdit(self.centralwidget)
+        self.lineEdit_pastelink.setObjectName(u"lineEdit_pastelink")
+
+        self.gridLayout_2.addWidget(self.lineEdit_pastelink, 0, 0, 1, 1)
+
+        self.pushButton_go = QPushButton(self.centralwidget)
+        self.pushButton_go.setObjectName(u"pushButton_go")
+
+        self.gridLayout_2.addWidget(self.pushButton_go, 0, 1, 1, 1)
+
+        self.stackedWidget = QStackedWidget(self.centralwidget)
+        self.stackedWidget.setObjectName(u"stackedWidget")
+        self.page_1 = QWidget()
+        self.page_1.setObjectName(u"page_1")
+        self.stackedWidget.addWidget(self.page_1)
+        self.page_2 = QWidget()
+        self.page_2.setObjectName(u"page_2")
+        self.gridLayout = QGridLayout(self.page_2)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.stackedWidget.addWidget(self.page_2)
+
+        self.gridLayout_2.addWidget(self.stackedWidget, 1, 0, 1, 2)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 21))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        self.stackedWidget.setCurrentIndex(1)
+
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.lineEdit_pastelink.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Paste the Link", None))
+        self.pushButton_go.setText(QCoreApplication.translate("MainWindow", u"GO", None))
+    # retranslateUi
+
